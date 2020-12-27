84e4e50503f40e34c1c9d48c8b438ed9ff1905b3
mappy1212 <Masayabasuke983@gmail.com>
2020-12-28 00:29:37 +0900

    function-1

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,23 @@
 class TweetsController < ApplicationController
   def index
+    @tweets = Tweet.includes(:user).order("created_at DESC")
+    
+  end
+
+  def show
+    @post = Tweet.find(params[:id])
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   
   root "tweets#index"
-  resources :users, only: [:index, :edit, :update]
-  resources :tweets, only: [:index, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :tweets, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end
